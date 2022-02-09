66b5e50f0725b805dbcb3f3fe5c03667913402d6
Iliyan <ili_kop3@abv.bg>
2022-02-09 21:38:45 +0200

    Finish project

--- js/form.js
+++ js/form.js
@@ -1,24 +1,70 @@
-function updatePrice(event) {
-	const prices = [
-		20,
-		40,
-		60,
-	];
+const prices = [
+	[ 20, 15 ],
+	[ 30, 25 ],
+	[ 40, 35 ],
+];
+const roomTaxes = [ 25, 50 ];
+
+function updatePrice() {
 	const images = [
 		"greenhill2.jpg",
 		"merianpalace1.jpg",
 		"parkhotel1.jpg",
 	];
-	const index = Number(event.target.value)
-	document.querySelector("#price").value = prices[index] + "лв.";
-	document.querySelector("#hotel-image").src = "Images/hotelsPhotos/" + images[index];
+
+	const hotelIndex = Number(document.querySelector("#hotel").value);
+	const price = prices[hotelIndex];
+	document.querySelector("#price").value = `За Възрастни: ${price[0]}лв. За Деца: ${price[1]}лв.`;
+	document.querySelector("#hotel-image").src = "Images/hotelsPhotos/" + images[hotelIndex];
+}
+
+function toggleCardForm(visible) {
+	const form = document.querySelector("#card");
+	form.style.display = visible ? "block" : "none";
+}
+
+function validate(number, text) {
+	const invalid = isNaN(number) && number > 0;
+	if (invalid)
+		alert(`Моля, въведете валиден брой ${text}!`);
+	return invalid;
 }
 
 function calculate(event) {
 	event.preventDefault();
-	const nights = Number(document.querySelector("#nights").value);
-	const price = Number(document.querySelector("#price").value.slice(0, -3));
-	if (isNaN(price))
+	const inputs = [
+		document.querySelector("#firstName"),
+		document.querySelector("#mobileNum"),
+		document.querySelector("#email"),
+		document.querySelector("#price"),
+		document.querySelector("#nights"),
+		document.querySelector("#children"),
+		document.querySelector("#adults"),
+	];
+
+	if (inputs.some(x => !x.value))
+		return alert("Моля, попълнете всички полета!");
+
+	const hotelIndex = Number(document.querySelector("#hotel").value); 
+	const roomIndex = Number(document.querySelector("#room").value); 
+	const price = Number(inputs[3].value.slice(0, -3));
+	const nights = Number(inputs[4].value);
+	const children = Number(inputs[5].value);
+	const adults = Number(inputs[6].value);
+
+	if (validate(nights, "нощувки"))
 		return;
-	document.querySelector("#result").innerText = (nights * price).toFixed(2) + "лв.";
+
+	if (validate(children, "деца"))
+		return;
+
+	if (validate(adults, "възрастни"))
+		return;
+
+	const adultsPrice = adults * prices[hotelIndex][0];
+	const childrenPrice = children * prices[hotelIndex][1];
+	const roomTax = roomTaxes[roomIndex];
+	const finalPrice = roomTax + nights * (adultsPrice + childrenPrice);
+	document.querySelector("#result").innerText = finalPrice.toFixed(2) + "лв.";
+	[ ...document.querySelectorAll(".payment-button") ].forEach(x => x.style.display = "block");
 }
---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(function() {
+
+    var owner = $('#owner');
+    var cardNumber = $('#cardNumber');
+    var cardNumberField = $('#card-number-field');
+    var CVV = $("#cvv");
+    var mastercard = $("#mastercard");
+    var confirmButton = $('#confirm-purchase');
+    var visa = $("#visa");
+    var amex = $("#amex");
+
+    cardNumber.payform('formatCardNumber');
+    CVV.payform('formatCardCVC');
+
+    confirmButton.click(function(e) {
+        e.preventDefault();
+        
+        var isCardValid = $.payform.validateCardNumber(cardNumber.val());
+        var isCvvValid = $.payform.validateCardCVC(CVV.val());
+
+        if(owner.val().length < 5) {
+            alert("Wrong owner name");
+        } else if (!isCardValid) {
+            alert("Wrong card number");
+        } else if (!isCvvValid) {
+            alert("Wrong CVV");
+        } else {
+            alert("Everything is correct");
+            location.href="index.html"
+        }
+    });
+});
